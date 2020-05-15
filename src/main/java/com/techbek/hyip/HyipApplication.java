44d2a7e27176cd8871615bb5720e22798c8b1c72
wizardoma <alibekason@gmail.com>
2020-05-15 08:01:39 +0100

    Initial commit

---
+++ src/main/java/com/techbek/hyip/HyipApplication.java
@@ -0,0 +1,13 @@
+package com.techbek.hyip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HyipApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/techbek/hyip/HyipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techbek.hyip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HyipApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

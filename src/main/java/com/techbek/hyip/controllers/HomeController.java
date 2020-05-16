be7969d2a3c9d9488ebb26e6c9a023fe833397d5
wizardoma <alibekason@gmail.com>
2020-05-16 16:12:53 +0100

    Realized homepage

--- src/main/java/com/techbek/hyip/controllers/HomeController.java
+++ src/main/java/com/techbek/hyip/controllers/HomeController.java
@@ -3,15 +3,14 @@ package com.techbek.hyip.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
     public String getHomePage(Model model){
-        String welcome = "Welcome to Hyip. Where Dreams come true";
-        model.addAttribute("test",welcome);
+        String docTitle = "Welcome to Hyip";
+        model.addAttribute("docTitle",docTitle);
         return "home";
     }
 }
